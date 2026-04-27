694d0fc879ba9a2d1b7c48f46e9b0af77e4fdbfd
y-chiba1008 <118287721+y-chiba1008@users.noreply.github.com>
2026-04-27 10:26:33 +0000

    feat: 解説をみて修正

--- problems/0001_two_sum/main.py
+++ problems/0001_two_sum/main.py
@@ -3,29 +3,10 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        val_to_idxs: dict[int, list[int]] = {}
-        for idx, val in enumerate(nums):
-            if val in val_to_idxs:
-                val_to_idxs[val].append(idx)
-            else:
-                val_to_idxs[val] = [idx]
+        val_to_idx = {val: idx for idx, val in enumerate(nums)}
+        for idx, n in enumerate(nums):
+            rest = target - n
+            if rest in val_to_idx and val_to_idx[rest] != idx:
+                return [idx, val_to_idx[rest]]
 
-        nums = sorted(nums)
-        idx_small = 0
-        idx_large = len(nums) - 1
-        while idx_small < idx_large:
-            sum_temp = nums[idx_small] + nums[idx_large]
-            if sum_temp > target:
-                idx_large -= 1
-            elif sum_temp < target:
-                idx_small += 1
-            else:
-                break
-
-        val_1 = nums[idx_small]
-        val_2 = nums[idx_large]
-
-        idx_1 = val_to_idxs[val_1].pop()
-        idx_2 = val_to_idxs[val_2].pop()
-
-        return [idx_1, idx_2]
+        return []
